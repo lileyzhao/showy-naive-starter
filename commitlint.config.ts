cca1662970ed2f564f68931005a8639bda2f3d39
LileyZhao <lileyzhao@gmail.com>
2024-06-20 10:58:30 +0800

    chore: add commit message conventions

---
+++ commitlint.config.ts
@@ -0,0 +1,10 @@
+import type { UserConfig } from '@commitlint/types'
+
+const config: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'build', 'revert', 'ci']],
+  },
+}
+
+export default config
