8d816a9a4e442e58b7e9e8e97cbb378185133713
LileyZhao <lileyzhao@gmail.com>
2024-06-20 20:30:50 +0800

    refactor: optimize ProfileDrawer component layout

--- src/layout/components/ProfileDrawer.vue
+++ src/layout/components/ProfileDrawer.vue
@@ -35,10 +35,7 @@ defineExpose({ show })
 </script>
 
 <template>
-  <n-drawer
-    v-model:show="drawerActive" display-directive="show" :width="325"
-    :on-after-leave="() => drawerActive = false"
-  >
+  <n-drawer v-model:show="drawerActive" display-directive="show" :width="325">
     <n-drawer-content :title="t('theme.themeDrawerTitle')" :native-scrollbar="false" closable>
       <n-divider title-placement="left" dashed>
         {{ t('theme.themeMode.title') }}
@@ -46,8 +43,8 @@ defineExpose({ show })
       <div class="flex-col-stretch gap-16px">
         <div class="i-flex-center">
           <n-tabs
-            :key="app.ThemeModeRaw" type="segment" size="small" class="relative w-214px"
-            :value="app.ThemeModeRaw" @update:value="handleDarkModeChange"
+            :key="app.ThemeModeRaw" type="segment" size="small" class="relative w-214px" :value="app.ThemeModeRaw"
+            @update:value="handleDarkModeChange"
           >
             <n-tab v-for="(_, key) in icons" :key="key" :name="key">
               <div flex items-center justify-center gap-8px>
