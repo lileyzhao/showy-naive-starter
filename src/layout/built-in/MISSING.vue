430e3c85d12d0172048d5c0c1ff1abb9e3951afe
LileyZhao <lileyzhao@gmail.com>
2024-07-02 22:27:14 +0800

    feat: optimize dynamic routing and add missing component

---
+++ src/layout/built-in/MISSING.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'MissingComponent',
+}
+</script>
+
+<template>
+  <div class="error">
+    <h2>组件缺失</h2>
+    <p>我们无法找到您请求的组件。请检查组件名称是否正确，或联系技术支持。</p>
+  </div>
+</template>
+
+<style>
+.error {
+  color: red;
+  text-align: center;
+}
+</style>
--- src/router/helper.ts
+++ src/router/helper.ts
@@ -1,13 +1,13 @@
-// Dynamic import of view modules 动态导入视图模块
-export const dynamicViewsModules: Record<string, () => Promise<Recordable>> = import.meta.glob('@/views/**/*.{vue,tsx}')
-
-// Add built-in pages 添加内置的页面
+// Built-in pages 内置的页面
 export const builtInViews: Record<string, () => Promise<Recordable>> = import.meta.glob('@/layout/built-in/**/*.{vue,tsx}')
 
-// Merge built-in views into dynamic view modules 合并内置页面到动态视图模块
-Object.assign(dynamicViewsModules, builtInViews)
+// Dynamic import of view modules 动态导入视图模块
+export const userViews: Record<string, () => Promise<Recordable>> = import.meta.glob('@/views/**/*.{vue,tsx}')
 
-console.log('%c 组件表 ', 'color:white;background-color:blue;', dynamicViewsModules)
+// Merge built-in views into dynamic view 合并内置页面到动态视图
+const dynamicViews = { ...builtInViews, ...userViews }
+
+console.log('%c 组件表 ', 'color:white;background-color:blue;', dynamicViews)
 
 // Cache the matching results 缓存匹配结果
 const matchCache = new Map<string, () => Promise<Record<string, any>>>()
@@ -20,33 +20,42 @@ export const dynamicImport = (component: string) => {
   }
 
   // Normalize the path and filter matching keys 处理路径并过滤匹配项
-  const normalizedComponent = component.replace(/.*?(views\/)/, '').replace(/.*?(layout\/built-in\/)/, '').replace(/(\.vue|\.tsx)$/, '')
-  const matchKeys = Object.keys(dynamicViewsModules).filter((key) => {
-    const normalizedKey = key.replace(/.*?(views\/)/, '').replace(/.*?(layout\/built-in\/)/, '').replace(/(\.vue|\.tsx)$/, '')
+  const normalizedComponent = component
+    .replace(/.*?(views\/)/, '')
+    .replace(/.*?(layout\/built-in\/)/, '')
+    .replace(/(\.vue|\.tsx)$/, '')
+  const matchKeys = Object.keys(dynamicViews).filter((key) => {
+    const normalizedKey = key
+      .replace(/.*?(views\/)/, '')
+      .replace(/.*?(layout\/built-in\/)/, '')
+      .replace(/(\.vue|\.tsx)$/, '')
     return normalizedKey === normalizedComponent
   })
 
   // Cache and return the import function if a unique match is found
-  // 如果找到唯一一个匹配项，缓存并返回对应的导入函数
-  if (matchKeys.length === 1) {
+  // 如果找到匹配项，缓存并返回对应的导入函数
+  if (matchKeys.length >= 1) {
+    if (matchKeys.length > 1) {
+      // Issue a warning if multiple matches are found 如果找到多个匹配项，发出警告
+      console.warn(
+        `Multiple matches found for component "${component}": ${matchKeys.join('\n')}. Defaulting to the first match: "${matchKeys[0]}". `,
+        +`组件 "${component}" 找到多个匹配项：${matchKeys.join('\n')}。 默认使用第一个匹配项: "${matchKeys[0]}"。`,
+      )
+    }
+
     const matchKey = matchKeys[0]
-    const matchedModule = dynamicViewsModules[matchKey]
+    const matchedModule = dynamicViews[matchKey]
     matchCache.set(component, matchedModule)
     return matchedModule
   }
-
-  // Issue a warning if multiple matches are found 如果找到多个匹配项，发出警告
-  if (matchKeys.length > 1) {
-    console.warn(
-      'Please do not create `.vue` and `.tsx` files with the same file name in the same hierarchical directory under the views folder. This will cause dynamic introduction failure. '
-      + '请不要在views文件夹下的同一层级目录中创建相同文件名的`.vue`和`.tsx`文件，这会导致动态引入失败。',
-    )
-  }
-  // Issue a warning if no match is found 如果没有找到匹配项，发出警告
   else {
+    // Issue a warning if no match is found 如果没有找到匹配项，发出警告
     console.warn(
       `Cannot find \`${component}.vue\` or \`${component}.tsx\` under src/views/, please create it yourself! `
       + `在src/views/下找不到\`${component}.vue\` 或 \`${component}.tsx\`, 请自行创建!`,
     )
+    const matchedModule = dynamicViews.MISSING
+    matchCache.set(component, matchedModule)
+    return matchedModule
   }
 }
