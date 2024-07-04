5e1c5179dbdf3921838233e3044c3b1ecd6b7e35
LileyZhao <lileyzhao@gmail.com>
2024-07-04 09:28:24 +0800

    chore: 🔧 update commitlint configuration

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,7 +3,9 @@ import type { UserConfig } from '@commitlint/types'
 const config: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'build', 'revert', 'ci']],
+    'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'perf', 'style', 'test', 'docs', 'build', 'ci', 'revert', 'chore']],
+    // The subject cannot end with certain symbols 主题不能以某些符号结尾
+    'subject-full-stop': [2, 'never', '/[.!?,;。！？，；]$/'],
   },
 }
 
