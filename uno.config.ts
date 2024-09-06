059fbcd42f624e1e1e6b585a00436846070e4761
LileyZhao <lileyzhao@gmail.com>
2024-09-06 17:49:45 +0800

    fix: 🐛 fix menu icon display

--- uno.config.ts
+++ uno.config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
         // the default
         /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
         // 从指定文件中提取图标，参考：https://unocss.dev/guide/extracting#extracting-from-build-tools-pipeline
-        'src/api-services/menuData.json',
+        'src/api/menuData.json',
         'src/router/index.ts',
         'src/shared/utils/menuUtil.ts',
       ],
